43dea34dcf1a94f38ca718aab31a82b75e782abd
xeonium54 <120768801+xeonium54@users.noreply.github.com>
2024-11-12 01:57:35 +0700

    Content Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: iLanding
-* Template URL: https://bootstrapmade.com/ilanding-bootstrap-landing-page-template/
-* Updated: Oct 28 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
